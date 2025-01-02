b45e653c63e10e6c93f552686711d371cd3422b8
Matt Booth <mattb@macbookpro.lan>
2025-01-02 11:28:21 +0000

    Prepare for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
-  define: {
-    'process.env.VITE_AIRTABLE_PAT': JSON.stringify(process.env.VITE_AIRTABLE_PAT),
-    'process.env.VITE_AIRTABLE_BASE_ID': JSON.stringify(process.env.VITE_AIRTABLE_BASE_ID),
-    'process.env.VITE_AIRTABLE_TABLE_ID': JSON.stringify(process.env.VITE_AIRTABLE_TABLE_ID),
-  }
+  build: {
+    outDir: 'dist',
+  },
 })
