0dcc8c624a9301e75450c7cbf956868399b703d4
Matt Booth <mattb@macbookpro.lan>
2025-01-02 11:52:52 +0000

    Update to ensure local and deployed versions auth with airtable

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
+  root: '.',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
