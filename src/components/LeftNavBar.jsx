5161cfa4363ef88d38df0356e72df429c253b731
Matt Booth <mattb@macbookpro.lan>
2025-01-07 09:08:16 +0000

    Fix to image referencing

--- src/components/LeftNavBar.jsx
+++ src/components/LeftNavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, VStack, Image, Text, Flex } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import Image from 'next/image';
 
 const LeftNavBar = () => {
   const navItems = [];
