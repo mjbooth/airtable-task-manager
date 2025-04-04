5d272cbe133c002e3c4d4bf46f3ddcfe0b36e97f
Matt Booth <mattb@macbookpro.lan>
2025-04-04 11:55:45 +0100

    Trigger Vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
   },
   resolve: {
     alias: {
